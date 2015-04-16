b59eb802d4ec50ec72aecca9808c82c221b035a8
adamian <adamianou@gmail.com>
2015-04-16 12:23:42 +0100

    Added comments in the core ABM

--- ABM/ABM.py
+++ ABM/ABM.py
@@ -9,12 +9,34 @@ import numpy as np
 # d={'first':1,'second':2}
 # a=ABM.pSAM(d)
 
+
+"""
+SAM based on Latent Feature Models
+"""
 class LFM(object):
     def __init__(self):
         self.__type = []
         self.model = []
 
     def store(self,observed, inputs=None, Q=None, kernel=None, num_inducing=None):
+        """
+        Store events.
+        ARG: obserbved: A N x D matrix, where N is the number of points and D the number
+        of features needed to describe each point.
+        ARG: inputs: A N x Q matrix, where Q is the number of features per input. 
+        Leave None for unsupervised learning.
+        ARG: Q: Leave None for supervised learning (Q will then be the dimensionality of
+        inputs). Otherwise, specify with Q the dimensionality (number of features) for the
+        compressed space that acts as "latent" inputs.
+        ARG: kernel: for the GP. can be left as None for default.
+        ARG: num_inducing: says how many inducing points to use. Inducing points are
+        a fixed number of variables through which all memory is filtered, to achieve
+        full compression. E.g. it can correspond to the number of neurons.
+        Of course, this is not absolutely fixed, but it also doesn't grow necessarily
+        proportionally to the data, since synapses can make more complicated combinations
+        of the existing neurons. The GP is here playing the role of "synapses", by learning
+        non-linear and rich combinations of the inducing points.
+        """
         assert(isinstance(observed,dict))
         self.observed = observed
         self.__num_views = len(self.observed.keys())
@@ -53,12 +75,25 @@ class LFM(object):
         self.model.data_labels = None
     
     def add_labels(self, labels):
+        """
+        If observables are associated with labels, they can be added here.
+        labels has to be a matrix of size N x K, where K is the total number
+        of different labels. If e.g. the i-th row of L is [1 0 0] (or [1 -1 -1])
+        then this means that there are K=3 different classes and the i-th row
+        of the observables belongs to the first class.
+        """
         self.model.data_labels = labels
 
     def learn(self, optimizer='bfgs',max_iters=1000, verbose=True):
+        """
+        Learn the model (analogous to "forming synapses" after perveiving data).
+        """
         self.model.optimize(optimizer, messages=verbose, max_iters=max_iters)
 
     def visualise(self):
+        """
+        Show the internal representation of the memory
+        """
         if self.__type == 'bgplvm' or self.__type == 'mrd':
             if self.model.data_labels is not None:
                 ret = self.model.plot_latent(labels=self.model.data_labels)
@@ -69,10 +104,22 @@ class LFM(object):
         return ret
 
     def recall(self, locations):
+        """
+        Recall stored events. This is closely related to performing pattern pattern_completion
+        given "training" data.
+        """
         # TODO
         pass
 
     def pattern_completion(self, test_data):
+        """
+        In the case of supervised learning, pattern completion means that we 
+        give new inputs and infer their correspondin outputs. In the case of
+        unsupervised leaerning, pattern completion means that we give new
+        outputs and we infer their corresponding "latent" inputs, ie the internal
+        compressed representation of the new outputs in terms of the already
+        formed "synapses".
+        """
         if self.__type == 'bgplvm':
             #tmp = self.model.infer_newX(test_data)[0]
             #pred_mean = tmp.mean
